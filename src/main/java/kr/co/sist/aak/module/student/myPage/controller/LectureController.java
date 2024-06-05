dba194530351725693d3e3ca640e35de09cb5455
onestar90 <mrb8215@gmail.com>
2024-06-05 19:33:05 +0900

    Feat: semantic UI 적용

--- src/main/java/kr/co/sist/aak/module/student/myPage/controller/LectureController.java
+++ src/main/java/kr/co/sist/aak/module/student/myPage/controller/LectureController.java
@@ -1,5 +1,17 @@
 package kr.co.sist.aak.module.student.myPage.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LectureController {
 // 학생 마이페이지 강의 관련 controller
+	@GetMapping("/test.do")
+	public String test(){
+		return "student/my_page/semantic_test";
+	}
+	@GetMapping("/login.do")
+	public String login(){
+		return "student/login_join";
+	}
 }
