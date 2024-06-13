9c25e93099f3b30082cc0d52de2e6bcd70e218ef
joyjelly <noop0728@naver.com>
2024-06-13 22:24:15 +0900

    강사추가 폼 손댐...

--- src/main/java/kr/co/sist/aak/module/admin/eduCategoryManagement/controller/EduCategoryManagementController.java
+++ src/main/java/kr/co/sist/aak/module/admin/eduCategoryManagement/controller/EduCategoryManagementController.java
@@ -1,13 +1,47 @@
 package kr.co.sist.aak.module.admin.eduCategoryManagement.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.sist.aak.domain.admin.EduCategoryManagementDomain;
+import kr.co.sist.aak.module.admin.eduCategoryManagement.Service.EduCategoryManagementService;
 
 @Controller
 public class EduCategoryManagementController {
+	@Autowired(required = false)
+	private EduCategoryManagementService ems;
+
+	/**
+	 * @param model
+	 * @return
+	 */
 	@GetMapping("manage_edu_cat.do")
-	public String searchCategoryList() {
-		
+	public String searchCategoryList(Model model) {
+		List<EduCategoryManagementDomain> list = null;
+		list = ems.searchCategoryList();
+		model.addAttribute("list", list);
+
 		return "/admin/manage_edu_cat";
 	}
+	
+	/**
+	 * @param model
+	 * @param cat_code
+	 * @return
+	 */
+	public String searchSubCategoryList(Model model, String cat_code) {
+		List<EduCategoryManagementDomain> list = null;
+		
+		list = ems.searchSubCate(cat_code);
+		model.addAttribute("list",list);
+		
+		
+		
+		return "/admin/manage_educategory/manage_subcat";
+	}
+
 }
--- src/main/java/kr/co/sist/aak/module/admin/instructorManagement/controller/InstructorManagementController.java
+++ src/main/java/kr/co/sist/aak/module/admin/instructorManagement/controller/InstructorManagementController.java
@@ -1,11 +1,18 @@
 package kr.co.sist.aak.module.admin.instructorManagement.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import kr.co.sist.aak.domain.admin.InstructorManagementDomain;
 import kr.co.sist.aak.module.admin.instructorManagement.service.InstructorManagementService;
@@ -14,38 +21,49 @@ import kr.co.sist.aak.module.admin.instructorManagement.service.InstructorManage
 public class InstructorManagementController {
 	@Autowired(required = false)
 	private InstructorManagementService ims;
-	
+
 	@GetMapping("manage_instructor_details.do")
-public String instructorDetail(Model model, String inst_id) {
-		InstructorManagementDomain imd = ims.instructorDetail(inst_id);
-		
-		String name =imd.getName();
-		String id =imd.getInst_id();
-		String email = imd.getEmail();
-		String intro = imd.getIntroduction();
-		String image = imd.getImage();
-		String phone = imd.getPhone();
-		String major_subject = imd.getMajor_subject();
-		String education = imd.getEducation();
-		model.addAttribute("ims",ims);
-		model.addAttribute("name",name);
-		model.addAttribute("id",id);
-		model.addAttribute("email",email);
-		model.addAttribute("intro",intro);
-		model.addAttribute("image",image);
-		model.addAttribute("phone",phone);
-		model.addAttribute("major_subject",major_subject);
-		model.addAttribute("education",education);
-	return "/admin/manage_instructor/manage_instructor_details";
-}
+	public String instructorDetail(InstructorManagementDomain imd, Model model, String inst_id) {
+		imd = ims.instructorDetail(inst_id);
+		model.addAttribute("imd", imd);
+
+		return "/admin/manage_instructor/manage_instructor_details";
+	}
+
 	@GetMapping("manage_instructor_addform.do")
-	public String instructorAdd() {
+	public String instructorAddForm() {
 		return "/admin/manage_instructor/manage_instructor_add";
 	}
+	public String instructorFormProcess( HttpServletRequest request,String temp, Model model) throws IOException {
+		File saveDir = new File("C:/dev/workspace_spring/spring_mvc/src/main/webapp/upload");
+		int tempSize =100*1024*1024;
+		//2. 파일 업로드 클래스 생성.
+		MultipartRequest mr = new MultipartRequest(request, saveDir.getAbsolutePath()
+				,tempSize,"UTF-8", new DefaultFileRenamePolicy());
+		//업로더 명 (web parameter)
+		String uploader = mr.getParameter("uploader");
+		String oriName = mr.getOriginalFileName("upfile");
+		String fsName = mr.getFilesystemName("upfile");
+		//최대크기 10mbyte
+		File tempFile = new File(saveDir.getAbsolutePath()+"/"+fsName);
+		int maxSize = 10*1024*1024;
+		System.out.println(System.getProperty("user.dir"));
+		//업로드 크기 제한
+		boolean uploadflag =false;
+		if(tempFile.length()>maxSize) {
+			tempFile.delete();
+			uploadflag=true;
+		}
+		model.addAttribute("uploader",uploader);
+		model.addAttribute("fileName",oriName);
+		model.addAttribute("uploadflag",!uploadflag);
+		return "";
+	}
+
 	@GetMapping("manage_instructor.do")
 	public String searchInstructorList(Model model) {
 		List<InstructorManagementDomain> list = ims.searchAllNInstructor();
-		model.addAttribute("instList",list);
+		model.addAttribute("instList", list);
 		return "/admin/manage_instructor";
 	}
 }
