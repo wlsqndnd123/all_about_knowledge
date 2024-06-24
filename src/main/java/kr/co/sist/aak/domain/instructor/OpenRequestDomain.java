1e58b4469b158df85c9ab8d6b37a9c3af97b98da
sumin091 <135329738+sumin091@users.noreply.github.com>
2024-06-24 12:48:47 +0900

    매퍼수정

--- src/main/java/kr/co/sist/aak/domain/instructor/OpenRequestDomain.java
+++ src/main/java/kr/co/sist/aak/domain/instructor/OpenRequestDomain.java
@@ -13,7 +13,7 @@ import lombok.ToString;
 @ToString
 public class OpenRequestDomain {
 
-	private String sub_code, lec_code, sub_title, intro, goal, image, lec_name, explain, file_Nm;
+	private String sub_code, lec_code, sub_title, intro, goal, image, lec_name;
 	private int total_no, total_time, lesson;
 	private Date create_date;
 }
