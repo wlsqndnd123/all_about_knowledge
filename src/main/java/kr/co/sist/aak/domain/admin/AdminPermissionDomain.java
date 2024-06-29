3c541d24f7cb8f73aa9659498d651df5807caeae
joyjelly <noop0728@naver.com>
2024-06-29 22:39:03 +0900

    컨피그 추가&&관리자 로그인

---
+++ src/main/java/kr/co/sist/aak/domain/admin/AdminPermissionDomain.java
@@ -0,0 +1,6 @@
+package kr.co.sist.aak.domain.admin;
+
+public class AdminPermissionDomain {
+	private String id , notice_management, member_management, category_management, instructor_management, qna_management;
+
+}
--- src/main/java/kr/co/sist/aak/module/admin/login/AdminLoginController.java
+++ src/main/java/kr/co/sist/aak/module/admin/login/AdminLoginController.java
@@ -1,13 +1,30 @@
 package kr.co.sist.aak.module.admin.login;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.co.sist.aak.domain.admin.vo.AdminLoginVO;
+
 @Controller
 public class AdminLoginController {
+	@Autowired(required = false)
+	private AdminLoginService adls;
+	
 	@RequestMapping(value = "adminindex.do",method = {RequestMethod.GET,RequestMethod.POST})
-public String login() {
-	return "/admin/index";
+public String login(@ModelAttribute AdminLoginVO alVO, Model model,HttpSession session) {
+	if(adls.searchLogin(alVO)) {
+	session.setAttribute("adminid", alVO.getId());	
+		return "/admin/index";
+	}else {
+		session.setAttribute("adminid", false);	
+		
+	}
+		return "/admin/login";
 }
 }
