f55e11ef5d46662ab7c6f2d8517c93139497b642
onestar90 <mrb8215@gmail.com>
2024-07-01 19:00:51 +0900

    Feat: 탈퇴 VO 추가

---
+++ src/main/java/kr/co/sist/aak/module/student/auth/controller/WithdrawalController.java
@@ -0,0 +1,5 @@
+package kr.co.sist.aak.module.student.auth.controller;
+
+public class WithdrawalController {
+
+}
---
+++ src/main/java/kr/co/sist/aak/module/student/auth/repository/WithdrawalRepository.java
@@ -0,0 +1,5 @@
+package kr.co.sist.aak.module.student.auth.repository;
+
+public class WithdrawalRepository {
+
+}
