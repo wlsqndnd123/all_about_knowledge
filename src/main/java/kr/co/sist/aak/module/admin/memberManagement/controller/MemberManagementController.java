161d28f7a3e084a448203579561e985122b4182d
jin <wlsqndnd123@naver.com>
2024-07-02 12:44:34 +0900

    저오류좀잡아주세요

--- src/main/java/kr/co/sist/aak/module/admin/memberManagement/controller/MemberManagementController.java
+++ src/main/java/kr/co/sist/aak/module/admin/memberManagement/controller/MemberManagementController.java
@@ -8,7 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.co.sist.aak.domain.admin.MemberManagementDomain;
 import kr.co.sist.aak.domain.admin.vo.MemberManagementVO;
@@ -53,8 +54,7 @@ public class MemberManagementController {
 			) {
 		
 		int cnt=0;	
-		
-		
+
 		cnt=mms.modyifyMember(mmVO);
 		model.addAttribute("cnt",cnt);
 		model.addAttribute("std_id",mmVO.getStd_id());
@@ -64,6 +64,12 @@ public class MemberManagementController {
 	
 	
 	
+	@RequestMapping(value = "manage_member_pre.do", method = RequestMethod.GET, 
+	produces = "application/json;charset=UTF-8")
+	public String searchMembers() {
+						
+		return mms.searchMembers();
+	}
 	
 	
 }
