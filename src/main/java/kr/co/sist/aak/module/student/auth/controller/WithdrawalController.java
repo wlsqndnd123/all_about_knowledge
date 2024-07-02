e6abb9d27c177e5ca72af6e9a706b470ea4bdcb4
Onestar90 <162802481+Onestar90@users.noreply.github.com>
2024-07-02 16:35:10 +0900

    Revert "One"

--- src/main/java/kr/co/sist/aak/module/student/auth/controller/WithdrawalController.java
+++ src/main/java/kr/co/sist/aak/module/student/auth/controller/WithdrawalController.java
@@ -1,31 +1,5 @@
 package kr.co.sist.aak.module.student.auth.controller;
 
-import java.security.Principal;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import kr.co.sist.aak.domain.student.vo.WithdrawalVO;
-import kr.co.sist.aak.module.student.auth.service.WithdrawalService;
-
-@Controller
 public class WithdrawalController {
-	
-    @Autowired(required = false)
-    private WithdrawalService withdrawalService;
-
-    @RequestMapping(value = "/mypage/withdrawal_form.do", method = RequestMethod.POST)
-    @ResponseBody
-    public String join(Principal principal,
-                       @RequestParam String reason) {
-    	String stdId = principal.getName();
-    	System.out.println("Received request to withdraw for student ID: " + stdId + " with reason: " + reason); // 로그 추가
-    	WithdrawalVO withdrawalVO = withdrawalService.createExitReason(stdId, reason);
-    	return "success";
-    }
 
 }
--- src/main/java/kr/co/sist/aak/module/student/auth/repository/WithdrawalRepository.java
+++ src/main/java/kr/co/sist/aak/module/student/auth/repository/WithdrawalRepository.java
@@ -1,29 +1,5 @@
 package kr.co.sist.aak.module.student.auth.repository;
 
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import kr.co.sist.aak.domain.student.vo.WithdrawalVO;
-import kr.co.sist.aak.util.SpringMyBatis;
-
-@Component
 public class WithdrawalRepository {
 
-	@Autowired(required = false)
-	private SpringMyBatis myBatisDAO;
-	
-	public void deleteStudent(String stdId) {
-		SqlSession ss = myBatisDAO.getMyBatisHandler(true);
-		ss.update("kr.co.sist.aak.student.auth.exit.deleteStudent", stdId);
-		myBatisDAO.closeHandler(ss);
-	}
-	
-	public void insertExitReason(WithdrawalVO withdrawalVO) {
-		SqlSession ss = myBatisDAO.getMyBatisHandler(true);
-		ss.insert("kr.co.sist.aak.student.auth.exit.insertExitReason", withdrawalVO);
-		myBatisDAO.closeHandler(ss);
-	}
-
-	
 }
