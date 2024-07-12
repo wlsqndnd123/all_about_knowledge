b64e070c7983115f73a8034a5f02a41c1c36f98d
onestar90 <mrb8215@gmail.com>
2024-07-12 16:11:27 +0900

    Feat: 마지막으로 수강한 강의 공지사항 불러오기

---
+++ src/main/java/kr/co/sist/aak/domain/student/vo/LectureNoticeVO.java
@@ -0,0 +1,15 @@
+package kr.co.sist.aak.domain.student.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class LectureNoticeVO {
+
+	private String title, writeDate;
+}
